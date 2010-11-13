c30581451fcca59da6a9ea3a3cbb133e2e1b5347
Repeat <repeat+gitcommit@gmail.com>
2010-11-13 14:38:54 +0800

    Apply v40rc1 en changes onto zh-TW files.

--- template/zh-TW/default/setup/strings.txt.pl
+++ template/zh-TW/default/setup/strings.txt.pl
@@ -27,15 +27,20 @@
 # Please keep the strings in alphabetical order by their name.
 
 %strings = (
-    any               => '任何',
-    blacklisted       => '(被列入黑名單)',
-    checking_for      => '檢查',
-    checking_dbd      => '檢查可用的 perl DBD 模組...',
+    any => '任何',
+    apachectl_failed => <<END,
+警告：無法檢查 Apache 的設定。當你並非以 ##root## 執行
+checksetup.pl 時，這有可能會發生。若要查看所遭遇到的問題，
+執行： ##command##
+END
+    blacklisted => '(被列入黑名單)',
+    checking_for => '檢查',
+    checking_dbd => '檢查可用的 perl DBD 模組...',
     checking_optional => '下列 perl 模組是選用的：',
-    checking_modules  => '檢查 perl 模組中...',
-    chmod_failed      => '##path##: 變更權限失敗： ##error##',
-    chown_failed      => '##path##: 變更擁有者失敗： ##error##',
-    commands_dbd      => <<EOT,
+    checking_modules => '檢查 perl 模組中...',
+    chmod_failed => '##path##: 變更權限失敗： ##error##',
+    chown_failed => '##path##: 變更擁有者失敗： ##error##',
+    commands_dbd => <<EOT,
 你必須執行以下指令之一（視你使用的資料庫而定）：
 EOT
     commands_optional => '安裝選用模組的指令：',
@@ -49,22 +54,22 @@ EOT
 附加元件必須要回傳它們的名稱，而不是 <code>1</code> 或數字。
 詳情請參閱 Bugzilla::Extension 。
 END
-    feature_auth_ldap         => 'LDAP 認證',
-    feature_auth_radius       => 'RADIUS 認證',
+    feature_auth_ldap => 'LDAP 認證',
+    feature_auth_radius => 'RADIUS 認證',
     feature_graphical_reports => '圖表式報告',
-    feature_html_desc         => '在產品／群組描述中使用較多的 HTML',
-    feature_inbound_email     => 'Inbound Email',
-    feature_jobqueue          => '信件佇列',
-    feature_jsonrpc           => 'JSON-RPC 介面',
-    feature_jsonrpc_faster    => '讓 JSON-RPC 較快',
-    feature_new_charts        => '新圖表',
-    feature_old_charts        => '舊圖表',
-    feature_mod_perl          => 'mod_perl',
-    feature_moving            => '在不同機器間搬移 Bugs',
-    feature_patch_viewer      => 'Patch Viewer',
-    feature_smtp_auth         => 'SMTP 認證',
-    feature_updates           => '自動更新通知',
-    feature_xmlrpc            => 'XML-RPC 介面',
+    feature_html_desc => '在產品／群組描述中使用較多的 HTML',
+    feature_inbound_email => 'Inbound Email',
+    feature_jobqueue => '信件佇列',
+    feature_jsonrpc => 'JSON-RPC 介面',
+    feature_jsonrpc_faster => '讓 JSON-RPC 較快',
+    feature_new_charts => '新圖表',
+    feature_old_charts => '舊圖表',
+    feature_mod_perl => 'mod_perl',
+    feature_moving => '在不同機器間搬移 Bugs',
+    feature_patch_viewer => 'Patch Viewer',
+    feature_smtp_auth => 'SMTP 認證',
+    feature_updates => '自動更新通知',
+    feature_xmlrpc => 'XML-RPC 介面',
 
     file_remove => '移除 ##name## 中...',
     file_rename => '將 ##from## 更名為 ##to## 中...',
@@ -95,6 +100,17 @@ EOT
 # Note: When translating these "modules" messages, don't change the formatting
 # if possible, because there is hardcoded formatting in
 # Bugzilla::Install::Requirements to match the box formatting.
+    modules_message_apache => <<END,
+***********************************************************************
+* APACHE 模組                                                         *
+***********************************************************************
+* 一般來說，當 Bugzilla 升級時，所有 Bugzilla 使用者必須清除他們瀏    *
+* 覽器中的庫存，否則 Bugzilla 會故障。如果你在 Apache 設定中啟用特    *
+* 定的模組（通常叫 httpd.conf 或 apache2.conf ）的話，當你升級        *
+* Bugzilla 的時候， Bugzilla 的使用者就不需要清除瀏覽器中的庫存。     *
+* 你需要要啟用的模組有：                                              *
+*                                                                     *
+END
     modules_message_db => <<EOT,
 ***********************************************************************
 * 存取資料庫                                                          *
@@ -125,6 +141,7 @@ EOT
     module_not_found => "沒有找到",
     module_ok => 'OK',
     module_unknown_version => "找到未知的版本",
+    no_such_module => "在 CPAN 上找不到叫 ##module## 的 Perl 模組。",
     ppm_repo_add => <<EOT,
 ***********************************************************************
 * 給 Windows 使用者                                                   *
